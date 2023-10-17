531e123fa13d322a767e7077d00e3bd848000cf7
Bhav Kushwaha <75977991+bhavkushwaha@users.noreply.github.com>
2023-10-17 17:03:06 +0530

    Fixing Bugs

---
+++ .history/components/Navbar_20231017165835.js
@@ -0,0 +1,79 @@
+function Navbar() {
+    const navbar = document.getElementById("Navbar");
+    const Container = document.createElement("div");
+    Container.classList.add("container");
+    Container.innerHTML = `
+    <div class="nav-left d-flex" id="navbarNav">
+    <a class="navbar-brand me-5" href="./">
+      <img
+        class="BrandLogo"
+        src="./Images/logo.png"
+        alt=""
+        srcset=""
+      />
+    </a>
+    <ul class="navbar-nav d-none d-lg-flex">
+      <li class="nav-item">
+        <a class="nav-link" href="./">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="about.html">About Us</a>
+      </li>
+      <li class="nav-item">
+      <a class="nav-link" href="services.html">Services</a>
+    </li>
+      <li class="nav-item">
+        <a class="nav-link" href="contact.html">Contact Us</a>
+      </li>
+    </ul>
+  </div>
+  <div class="nav-right d-flex align-items-center">
+    
+    <button
+      class="btn collapsed d-lg-none"
+      type="button"
+      data-bs-toggle="offcanvas"
+      data-bs-target="#offcanvasRight"
+      aria-controls="offcanvasRight"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
+  </div>
+  <div
+    class="offcanvas offcanvas-end"
+    tabindex="-1"
+    id="offcanvasRight"
+    aria-labelledby="offcanvasRightLabel"
+  >
+    <div class="offcanvas-header">
+      
+      <button
+        type="button"
+        class="btn-close"
+        data-bs-dismiss="offcanvas"
+        aria-label="Close"
+      ></button>
+    </div>
+    <div class="offcanvas-body">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="./">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="about.html">About Us</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="services.html">Services</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="contact.html">Contact Us</a>
+        </li>
+      </ul>
+      
+    </div>
+  </div>
+      `;
+    navbar.appendChild(Container);
+  }
+  Navbar();
+  
