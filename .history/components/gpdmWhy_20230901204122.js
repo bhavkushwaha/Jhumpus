f07f54dc56e5129d1876f2a3b359a96e56c6ae5b
Bhav Kushwaha <75977991+bhavkushwaha@users.noreply.github.com>
2024-04-30 12:31:53 +0530

    Delete .history directory

--- .history/components/gpdmWhy_20230901204122.js
+++
@@ -1,9 +0,0 @@
-function gpdmFeatures() {
-    const gpdmFeatures = document.getElementById("gpdmFeatures");
-    const Container = document.createElement("div");
-    Container.classList.add("container");
-    Container.innerHTML = `
-    `;
-    gpdmFeatures.appendChild(Container);
-  }
-  gpdmFeatures();
